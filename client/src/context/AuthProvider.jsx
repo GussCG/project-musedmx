aaa5ae628c64eb66e5e5657799fcfd9b25ddcced
Guss_CG <gcerdag1700@alumno.ipn.mx>
2025-03-31 00:10:22 -0600

    Laptop

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -25,12 +25,8 @@ function AuthProvider({ children }) {
     apPaterno: "Perez",
     apMaterno: "Gonzalez",
     email: "mail@mail.com",
-    tipoUsuario: 2,
-<<<<<<< HEAD
+    tipoUsuario: 1,
     fecNac: "2002-09-30T05:00:00.000Z",
-=======
-    fecNac: "2002-09-30",
->>>>>>> 3eadfe7fe3e1b929199eebfb6b888d165cb92ebb
     tel: "+525527167255",
     foto: "https://a.espncdn.com/i/headshots/nba/players/full/3975.png",
 
