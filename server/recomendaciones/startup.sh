d71fe7f4ed89744bd34496fdeab653565336ddca
GussCG <guxgux43@hotmail.com>
2025-06-07 14:39:42 -0600

    recomendaciones

---
+++ server/recomendaciones/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd server
+
+exec uvivcorn app:app --host=0.0.0.0 --port=8000
--- server/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd server
-
-exec uvivcorn recomendaciones.app:app --host=0.0.0.0 --port=8000
