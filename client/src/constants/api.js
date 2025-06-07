98b1111ba97a50bfdc61944ba4b05f86069c1628
GussCG <guxgux43@hotmail.com>
2025-06-07 02:47:51 -0600

    xd

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,6 +1,1 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-const BACKEND_URL = process.env.VITE_BACKEND_URL;
-
-console.log("BACKEND_URL:", BACKEND_URL);
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
