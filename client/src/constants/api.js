a8d546327b6caaa4903669d0537a223f620ee64e
GussCG <guxgux43@hotmail.com>
2025-06-07 02:35:45 -0600

    xd

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,3 +1,6 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+import dotenv from "dotenv";
+dotenv.config();
+
+const BACKEND_URL = process.env.VITE_BACKEND_URL;
 
 console.log("BACKEND_URL:", BACKEND_URL);
