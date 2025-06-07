26cb42cdf10e218b81e57af58b7f5c9e998235cf
GussCG <guxgux43@hotmail.com>
2025-06-07 17:31:39 -0600

    Recomendaciones otro repo

--- server/recomendaciones/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Agregar la ruta al directorio `server` como PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-printf "PYTHONPATH: %s\n" "$PYTHONPATH"
-
-# Ejecutar el servidor apuntando a recomendaciones.app
-exec uvicorn recomendaciones.app:app --host=0.0.0.0 --port=8000 --reload
