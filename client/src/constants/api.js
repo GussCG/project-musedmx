aae7a03b7428e67d76cc5b67e94eaf55cc73dc18
GussCG <guxgux43@hotmail.com>
2025-06-07 02:32:21 -0600

    xd

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = "https://musedmx-back-api.azurewebsites.net/";
+export const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
+console.log("BACKEND_URL:", BACKEND_URL);
