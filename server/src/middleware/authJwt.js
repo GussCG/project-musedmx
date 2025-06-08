0c764dd0241f297388dec02a5ce6c60ab5ce8197
GussCG <guxgux43@hotmail.com>
2025-06-07 20:02:04 -0600

    cookie

--- server/src/middleware/authJwt.js
+++ server/src/middleware/authJwt.js
@@ -1,6 +1,8 @@
 export const authMiddleware = (req, res, next) => {
   const token = req.cookies?.token;
 
+  console.log("Token recibido:", token);
+
   if (!token) {
     return res.status(401).json({ message: "Token no proporcionado" });
   }
