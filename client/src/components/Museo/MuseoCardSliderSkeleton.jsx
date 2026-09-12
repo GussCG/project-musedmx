124fb308721393145df83f98f61eefddf2a7f31f
Guss_CG <56102886+GussCG@users.noreply.github.com>
2026-09-11 23:20:27 -0600

    fix: Estilo Arreglado Index

--- client/src/components/Museo/MuseoCardSliderSkeleton.jsx
+++ client/src/components/Museo/MuseoCardSliderSkeleton.jsx
@@ -1,15 +1,22 @@
-import React from "react";
 import Skeleton from "react-loading-skeleton";
 
 function MuseoCardSliderSkeleton() {
   return (
     <div style={{ marginBottom: "20px", width: "100%" }}>
-      <Skeleton height={450} borderRadius={30} />
+      <Skeleton height={450} borderRadius={20} />
 
       <div style={{ marginTop: "15px" }}>
-        <Skeleton width={30} height={15} />
-        <Skeleton width={"90%"} height={20} style={{ marginTop: "10px" }} />
-        <Skeleton width={"80%"} height={20} style={{ marginTop: "10px" }} />
+        <Skeleton width={30} height={15} style={{ marginLeft: "10px" }} />
+        <Skeleton
+          width={"90%"}
+          height={20}
+          style={{ marginTop: "10px", marginLeft: "10px" }}
+        />
+        <Skeleton
+          width={"80%"}
+          height={20}
+          style={{ marginTop: "10px", marginLeft: "10px" }}
+        />
       </div>
     </div>
   );
